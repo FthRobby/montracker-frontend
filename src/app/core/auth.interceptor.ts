fa82613950f5f24fbc6c584c2e45ed5a71b32b0a
Kenzo <rbysalam10@gmail.com>
2026-08-03 18:03:08 +0700

    feat: enhance auth interceptor to handle 401 errors and logout

--- src/app/core/auth.interceptor.ts
+++ src/app/core/auth.interceptor.ts
@@ -1,7 +1,13 @@
 import { HttpInterceptorFn } from '@angular/common/http';
+import { inject } from '@angular/core';
+import { catchError, throwError } from 'rxjs';
+import { Router } from '@angular/router';
+import { AuthService } from './auth.service';
 
 export const authInterceptor: HttpInterceptorFn = (req, next) => {
   const token = localStorage.getItem('auth_token');
+  const router = inject(Router);
+  const authService = inject(AuthService);
 
   if (token) {
     req = req.clone({
@@ -9,5 +15,13 @@ export const authInterceptor: HttpInterceptorFn = (req, next) => {
     });
   }
 
-  return next(req);
+  return next(req).pipe(
+    catchError((error) => {
+      if (error.status === 401) {
+        authService.logout();
+        router.navigate(['/unauthorized'])
+      }
+      return throwError(() => error)
+    })
+  )
 };
